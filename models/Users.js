1f9d40b246c633be674fe5764461fbdde0bfaad3
Corey Donahue <cdartswebdev@gmail.com>
2024-02-27 13:30:05 -0700

    updated the username variable to userName

--- models/Users.js
+++ models/Users.js
@@ -20,6 +20,10 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
