8dc1aa504846f8365d75a5307a6eb13429b85969
Corey Donahue <cdartswebdev@gmail.com>
2024-03-12 17:33:00 -0600

    updated homerouts.js to allow for hyperlink functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,6 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
     const blogData = await BlogPost.findAll({
       include: [
         {
@@ -14,10 +13,8 @@ router.get("/", async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render("homepage", {
       blogs,
       logged_in: req.session.logged_in,
@@ -29,19 +26,33 @@ router.get("/", async (req, res) => {
 
 router.get("/blog/:id", async (req, res) => {
   try {
-    const blogData = await Blog.findByPk(req.params.id, {
+    const blogData = await BlogPost.findByPk(req.params.id, {
       include: [
         {
           model: User,
           attributes: ["name"],
         },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+        },
       ],
     });
 
-    const project = blogData.get({ plain: true });
+    if (!blogData) {
+      res.status(404).json({ message: "No blog post found with this id!" });
+      return;
+    }
 
-    res.render("blog", {
-      ...project,
+    const blog = blogData.get({ plain: true });
+
+    res.render("partials/dashboardPost", {
+      ...blog,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -49,28 +60,7 @@ router.get("/blog/:id", async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get("/dashboard", withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: BlogPost }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render("dashboard", {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
@@ -80,7 +70,6 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/create", withAuth, (req, res) => {
-  //
   res.render("create");
 });
 
