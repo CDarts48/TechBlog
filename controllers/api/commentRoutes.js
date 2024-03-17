5eab6d7d7fc75b596cb6cb8e226d3eee830dfd55
Corey Donahue <cdartswebdev@gmail.com>
2024-03-16 21:21:40 -0600

    comments log to DB need to fix rendering

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,8 +7,8 @@ router.post("/", async (req, res) => {
   try {
     console.log("we made it");
     const comment = await Comment.create({
-      comment_body: req.body.comment_body,
-      blogPost_id: req.body.blogPost_id,
+      comment: req.body.comment_body,
+      blog_id: req.body.blogPost_id,
       user_id: req.session.user_id || req.body.user_id,
     });
 
